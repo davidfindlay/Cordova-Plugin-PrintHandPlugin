039ef721df3570cde1605a1a2cd761dffecdabd3
David Findlay <davidjwfindlay@gmail.com>
2019-07-30 15:43:31 +1000

    Small fixes for compatability in Ionic Platform.

--- www/printhandplugin.js
+++ www/printhandplugin.js
@@ -1,20 +1,19 @@
-
-var exec = require('cordova/exec');
+// var exec = require('cordova/exec');
 
 var PrintHandPlugin = {
-     printWithHttpURL :function(callBackSuccess,callBackFail,webUrl) {
-     exec(callBackSuccess,callBackFail,"ShareIntentPrintHandPlugin","printWithHttpURL", [webUrl]);
-   },
-    printWebPageWithContain :function(callBackSuccess,callBackFail,htmlFormatString) {
-     exec(callBackSuccess,callBackFail,"ShareIntentPrintHandPlugin","printWebPageWithContain", [htmlFormatString]);
-   },
-  printImage :function(callBackSuccess,callBackFail,imagePath,activeView) {
-     /*Active View set true/false*/
-     exec(callBackSuccess,callBackFail,"ShareIntentPrintHandPlugin","printImage", [imagePath,activeView]);
-   },
-  printFile :function(callBackSuccess,callBackFail,imagePath,activeView,mineType) {
-    exec(callBackSuccess,callBackFail,"ShareIntentPrintHandPlugin","printFile", [imagePath,activeView,mineType]);
-   }
+    printWithHttpURL: function (callBackSuccess, callBackFail, webUrl) {
+        cordova.exec(callBackSuccess, callBackFail, "ShareIntentPrintHandPlugin", "printWithHttpURL", [webUrl]);
+    },
+    printWebPageWithContain: function (callBackSuccess, callBackFail, htmlFormatString) {
+        cordova.exec(callBackSuccess, callBackFail, "ShareIntentPrintHandPlugin", "printWebPageWithContain", [htmlFormatString]);
+    },
+    printImage: function (callBackSuccess, callBackFail, imagePath, activeView) {
+        /*Active View set true/false*/
+        cordova.exec(callBackSuccess, callBackFail, "ShareIntentPrintHandPlugin", "printImage", [imagePath, activeView]);
+    },
+    printFile: function (callBackSuccess, callBackFail, imagePath, activeView, mineType) {
+        cordova.exec(callBackSuccess, callBackFail, "ShareIntentPrintHandPlugin", "printFile", [imagePath, activeView, mineType]);
+    }
 };
 module.exports = PrintHandPlugin;
 
